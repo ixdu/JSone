d3b4d44f627451b463a73f61fd6056239a749c1c
Nikita Zaharov aka ix <ix@2du.ru>
2014-02-22 01:02:01 +0600

    example now is prototype of sphere ui. It will be diverted in dsa.ui services and sphere.ui in the future

--- examples/ideas_and_drafts.js
+++ examples/ideas_and_drafts.js
@@ -1,4 +1,4 @@
-var canvas = {
+var ui = {
     "frame" : null,
     "elements" : [],
     "in" : {
@@ -23,8 +23,10 @@ var canvas = {
 	    elements.push(element);
 	    dsa.send(client, 'new_element', element.id);
 	},
-	"fill" : function(client, element, ui_item){
+	"fill_element" : function(client, element, ui_item){
 	    //парсим тут дерево и реализуем разные элементы:)
+	},
+	"hide_element" : function(cleint, element){
 	}
     },
     "out" : {
@@ -40,17 +42,21 @@ var canvas = {
 }
 
 var address_panel = {
+    "_element" : '',
+    "address" : '',
     "in" : {
-	"show" : function(){
+	"init" : function(){
 	    //просим выделения элемента у канваса
-	    dsa.send('sphere.ui.canvas', 'give_element');
+	    dsa.send('ui', 'give_element');
 	},
 	"visible_changed" : function(client, state){
+	    //важно что поверхности, они же element, отключаются ui рекурсивно, так что может и не надо будет ничего делать
 	    //тут приостанавливаем или возобновляем свою визуальную или иную активность
 	},
 	"new_element" : function(client, element){
 	    //получаем запрошенный элемент. Примичательно, что можем получить новый в результате удаления
 	    //старого и создания нового без нашего ведома:)
+	    _element = element;
 	    var ui_item = {
 		"type" : 'image',
 		"color" : 'white',
@@ -61,7 +67,7 @@ var address_panel = {
 				"x" : "3%",
 				"y" : "1%",
 				"width" : "75%",
-				"heigth" : "98%"
+				"heigth" : "10%"
 		    },
 		    "gobutton" : {
 			"type" : 'button',
@@ -69,51 +75,135 @@ var address_panel = {
 			"x" : "78%",
 			"y" : "1%",
 			"width" : "20%",
-			"heigth" : "98%"
+			"heigth" : "10%"
 		    }
 		}
 	    }
-	    dsa.send('sphere.ui.canvas', 'fill', element, ui_item);
+	    dsa.send('ui', 'fill_element', element, ui_item);
 	},
 	"typed" : function(client, item, typed){
 	    //когда набирают в entry, люди приходят сообщения с тем, что уже набрано. В дальнейшем можно
 	    //запросить всё, что было набрано
+	    address = typed;	    
 	},
 	"pressed" : function(client, item, state){
-	    //приходит при нажатии кнопки, сообщает о состоянии нажатой кнопки - pressed, unpressed
+	    if(state == 'pressed')
+		dsa.send('sphere.ui.area_primary', 'open', address)
 	}
     }
+}
+
+var area = {
+    "_name" : null,
+    "object" : null,
+    "_address" : null,
+    "_element" : null,
+    "in" : {
+	"init" : function(name){
+	    _name = name;
+	    //нужно регистрировать себя в соответствии с именем
+	    dsa.send('ui', 'give_element');
+	},
+	"visible_changed" : function(client, state){
+	    //важно что поверхности, они же element, отключаются ui рекурсивно, так что может и не надо будет ничего делать
+	    //тут приостанавливаем или возобновляем свою визуальную или иную активность
+	},
+	"new_element" : function(client, element){
+	    _element = element;
+	    object = 'sphere.object.welcome';
+	    dsa.send('sphere.objects.welcome', 'take', element);
+	},
+	"resize" : function(client){
+	  //изменяем размер либо до максимального либо до нормального, но только для primary  
+	},
+	"open" : function(client, address){
+	    dsa.send(object, 'release');
+	    _address = object = address;
+	    dsa.send(address, 'take', element);
+	}
+    }    
+}
+
+var action_panel = {
+    "_element" : null,
+    "in" : {
+	"init" : function(){
+	    dsa.send('ui', 'give_element');
+	},
+	"visible_changed" : function(client, state){
+	    //важно что поверхности, они же element, отключаются ui рекурсивно, так что может и не надо будет ничего делать
+	    //тут приостанавливаем или возобновляем свою визуальную или иную активность
+	},
+	"new_element" : function(client, element){
+	    _element = element;
+	    var ui_item = {
+		"type" : 'image',
+		"color" : 'red',
+		"childs" : {
+		    "resize" :{
+			"type" : 'button',
+			"color" : 'blue',
+			"x" : "3%",
+			"y" : "1%",
+			"width" : "28%",
+			"heigth" : "10%"
+		    },
+		    "edit" : {
+			"type" : 'button',
+			"color" : 'blue',
+			"x" : "78%",
+			"y" : "1%",
+			"width" : "28%",
+			"heigth" : "10%"
+		    },
+		    "create" : {
+			"type" : 'button',
+			"color" : 'blue',
+			"x" : "3%",
+			"y" : "1%",
+			"width" : "28%",
+			"heigth" : "10%"
+		    }
+		}
+	    }
+	    dsa.send('ui', 'fill_element', element, ui_item);	    
+	},
+	"pressed" : function(client, item, state){
+	    if(state == 'pressed')
+		switch(item){
+		    case 'resize' :
+		    dsa.send('sphere.ui.area_primary', 'resize');
+		    break;
+		    case 'edit' : 
+		    dsa.send('sphere.ui.area_primary', 'open', 'sphere.ui.editor');
+		    break;
+		    case 'create' :
+		    dsa.send('sphere.ui.area_primary', 'open', 'sphere.ui.editor_new');
+		    break;
+		}
+	}
+    }    
+}
+
+var editor = {
+}
+
+var editor_new = {
+    
 }
 
 var sphere_ui = {
     "in" : {
 	"show" : function(){
-	    dsa.send('sphere.ui.canvas', 'show');
-	    dsa.send('sphere.ui.address_panel', 'show');
-	    dsa.send('sphere.ui.slave_area', 'show');
-	    dsa.send('sphere.ui.primary_area', 'show');
-	    dsa.send('sphere.ui.action_pane', 'show');
-	    //	    action_panel(maximize<->normalize, edit, create)
+	    dsa.send('ui', 'show');
+	    dsa.send('sphere.ui.address_panel', 'init');
+	    dsa.send('sphere.ui.area', 'init', '_primary');
+	    dsa.send('sphere.ui.area', 'init', '_slave');
+	    dsa.send('sphere.ui.action_panel', 'init');
 	},
 	"hide" : function(){
-	    dsa.send('sphere.ui.canvas', 'hide');
+	    dsa.send('ui', 'hide');
 	}	    
     }
 }
 
-var worker = {
-    "in" : {
-	"do" : function(){
-	    var sui = dsa.find('sphere.ui');
-	    dsa.send(sui, 'show');
-
-	    var elem_id = 'some id';
-	    var elements = dsa.find('sphere.elements');
-	    dsa.on(elements, 'element', function(element){
-		       
-		   })
-	    dsa.send(elements, 'query_by_id', elem_id);
-	}
-    }
-}
-\
